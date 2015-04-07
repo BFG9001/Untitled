6f1466e3e32fb1610a7775042f11cf2d357ae2c7
Abinox01 <noe.zaabi@gmail.com>
2015-04-07 21:09:27 +0200

    Update shared.lua
    I don't know if i really understand the concept but if i understand here is a team setup

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -4,3 +4,8 @@ GM.Email = ""
 GM.Website = ""
 
 DeriveGamemode( "sandbox" )
+
+
+team.SetUp( 0, "Loading", Color(0, 0, 255) )
+team.SetUp( 1, "Police", Color(255, 0, 0) )
+team.SetUp( 2, "Gangstar", Color(0, 255, 0) )
